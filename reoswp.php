626b7472b3e34690da54bc7a3d2a4cc8b5e7c8c8
Blayne <blayneholland@outlook.com>
2015-10-29 15:58:34 -0400

    added some very simple styles

--- reoswp.php
+++ reoswp.php
@@ -1,37 +1,25 @@
 <?php
-
-	require_once('reoswp/includes/const.php');
-	require_once('reoswp/includes/transaction.php'); 
-
-	//                     Content based on user role                          //
-// ======================================================================= //      
-
- require_once("templates/content-by-office.php");
-
-// ======================================================================= //
-
+	require_once('includes/const.php');
+	require_once('includes/transaction.php'); 
 ?>
 
 
 
-
-		<!DOCTYPE html>
+<!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
 
 	<head> 
 		<title>REO Intranet</title> 
 		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-		<meta name="description" content="REO" /> 
-		<meta name="keywords" content="REO" />
 		<link rel="stylesheet" type="text/css" href="css/sk_grid.css"> <!-- Docs. - http://getskeleton.com/ -->
 		<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
-		<link href="css/reo_05132015.css" rel="stylesheet" type="text/css">
 		<script type="text/javascript" src="js/vendor/parsley.min.js"></script>
+		<link rel="stylesheet" type="text/css" href="css/vendor/font-awesome/css/font-awesome.min.css">
 
 		<title>REO Payment System</title>
 
-		<link rel="stylesheet" type="text/css" href="reoswp/css/vendor/sk_grid.css" />
-		<link rel="stylesheet" type="text/css" href="reoswp/css/main.css" />
+		<link rel="stylesheet" type="text/css" href="css/vendor/sk_grid.css" />
+		<link rel="stylesheet" type="text/css" href="css/main.css" />
 		<script type="text/javascript" src="https://checkout.forte.net/v1/js"> </script>
 
 	</head>
@@ -40,40 +28,32 @@
 
 	  	<!-- REO Header Template      
  		============================= -->
-  		<?php require_once("templates/reo_header_05132015.html") ?>
 
 		<div class="i_container">
 
 		
 			<div class="sk-container">
 
-				<h1> REO Online Payment </h1>
 
 				<div class="pay-flash-message"> <!-- When transaction is complete user will see flash message --> </div>
 
 
 				<section class="pay_category">
 
-					<h2 class="pay-section-header"> Quick Pay </h2>
-
-
-					<p class="pay-section-description">
-						Use this option to make a payment on account. 
-						Enter the AMOUNT DUE, as shown on the Real Estate One Invoice/Statement, in the box below and click the Pay Now button. 
-						Additionally, payments greater than the amount due are also accepted (pay in advance, carry a credit balance, and not worry about due dates, late fees and interest). 
-						See additional terms and conditions below. 
-					</p>
+				<p class="pay-section-description">
+					Real Estate One is proud to offer another payment alternative for our Sales Associates.  
+					Below you will find several options to pay your accounts receivable balances by credit card or bank draft (where applicable) through PayPal.
+					Please note that by using any of the payment options listed below, you agree to the Terms and Conditions listed at the bottom of this page.
+				</p>
 
 
 					<div class="row">
 
-						<div class="twelve columns">
+						<div class="four columns">
 
-							<div class="pay-content-box">
+							<div class="pay-content-box box-small">
 								<!-- Pay Now -->
 								<h3> Quick Pay </h3>
-								<h3> [ Enter amount in checkout ] </h3>
-
 
 								<button api_login_id="<?php echo APIKEY ?>"
 									method="sale"
@@ -91,6 +71,20 @@
 								</button>
 
 							</div>
+
+						</div>
+
+						<div class="eight columns">
+
+							<h2 class="pay-section-header"> Quick Pay </h2>
+
+
+							<p>
+								Use this option to make a payment on account. 
+								Enter the AMOUNT DUE, as shown on the Real Estate One Invoice/Statement, in the box below and click the Pay Now button. 
+								Additionally, payments greater than the amount due are also accepted (pay in advance, carry a credit balance, and not worry about due dates, late fees and interest). 
+								See additional terms and conditions below. 
+							</p>
 
 						</div>
 
@@ -112,17 +106,21 @@
 
 					<div class="row">
 							<!-- Subscribe -->
-						<div class="one-third column">
+						<div class="four columns">
 
-							<div class="pay-content-box">
+							<div class="pay-content-box box-large">
 
-								<h3> Service Pack 1 </h3>
+								<h3> Option 1 </h3>
 
-								<h3> $72.00<span>&#47;mo</span> </h3>
+								<h3 class="price-header"> 
+									<span>$</span>72.00 <br />
+									<span>per month</span> 
+								</h3>
 
 								<ul>
-									<li> Tech Services </li>
-									<li> Legal Defense </li>
+									<li> <i class="fa fa-check"> </i> Tech Services </li>
+									<li> <i class="fa fa-check"> </i> Legal Defense </li>
+									<li> <i class="fa fa-times"> </i> Realtor.com </li>
 								</ul>
 
 								<!-- ====================== PAYMENT BUTTON ======================== -->
@@ -156,20 +154,23 @@
 						</div>
 
 
-						<div class="one-third column">
+						<div class="four columns">
 
 						
 							<!-- Subscribe -->
-							<div class="pay-content-box">
+							<div class="pay-content-box box-large">
 
-								<h3> Service Pack 2</h3>
+								<h3> Option 2</h3>
 
-								<h3> $99.00<span>&#47;mo</span> </h3>
+								<h3 class="price-header"> 
+									<span>$</span>99.00 <br />
+									<span>per month</span>
+								</h3>
 
 								<ul>
-									<li> Tech Services </li>
-									<li> Legal Defense </li>
-									<li> Realtor.com </li>
+									<li> <i class="fa fa-check"> </i> Tech Services </li>
+									<li> <i class="fa fa-check"> </i> Legal Defense </li>
+									<li> <i class="fa fa-check"> </i> Realtor.com </li>
 								</ul>
 
 
@@ -197,12 +198,11 @@
 						</div>
 
 
-						<div class="one-third column">
+						<div class="four columns">
 
-							<div class="pay-content-box">
+							<div class="pay-content-box box-small">
 
 								<h3> Custom Monthly Payment </h3>
-								<h3> [ Enter amount in checkout ] </h3>
 
 
 								<button api_login_id="<?php echo APIKEY ?>"
@@ -231,6 +231,13 @@
 
 					</div> <!-- row end -->
 
+					<p class="pay-section-description">
+						Subscribe using another amount.  This is useful if your receivable balance runs a little higher than one of the options above. 
+						Enter the amount you wish to have deducted below and have the amount automatically deducted each month. 
+						See additional terms and conditionsbelow.
+					</p>
+
+
 				</section>
 
 
@@ -249,11 +256,11 @@
 						<div class="three columns">
 
 						
-							<div class="pay-content-box">
+							<div class="pay-content-box box-small">
 
 								<h3> Realtor.com </h3>
 
-								<h3> 11 x $27 </h3>
+								<h3 class="price-header"> <span>$</span>27<span>x11</span> </h3>
 
 								<button api_login_id="<?php echo APIKEY ?>"
 									method="sale"
@@ -266,7 +273,7 @@
 									callback="oncallback_sale"
 									total_amount="<?php echo $annual_realtor->total_amount ?>"
 									order_number="<?php echo $annual_realtor->order_number ?>">
-								    <span> Pay Now </span> 
+								    <span> Buy Now </span> 
 
 
 								</button>
@@ -277,11 +284,11 @@
 
 						<div class="three columns">
 
-							<div class="pay-content-box">
+							<div class="pay-content-box box-small">
 
 								<h3> Annual Tech Service </h3>
 
-								<h3> 11 x $35 </h3>
+								<h3 class="price-header"><span>$</span>35<span>x11<span> </h3>
 
 								<button api_login_id="<?php echo APIKEY ?>"
 									method="sale"
@@ -294,7 +301,7 @@
 									callback="oncallback_sale"
 									total_amount="<?php echo $annual_tech->total_amount ?>"
 									order_number="<?php echo $annual_tech->order_number ?>" >
-								    <span> Pay Now </span>
+								    <span> Buy Now </span>
 								</button>
 
 							</div>
@@ -303,11 +310,11 @@
 
 						<div class="three columns">
 
-							<div class="pay-content-box">
+							<div class="pay-content-box box-small">
 
 								<h3> Annual Legal Defense </h3>
 
-								<h3> 11 x $37 </h3>
+								<h3 class="price-header"> <span>$</span>37<span>x11</span> </h3>
 
 								<button api_login_id="<?php echo APIKEY ?>"
 									method="sale"
@@ -320,7 +327,7 @@
 									callback="oncallback_sale"
 									total_amount="<?php echo $annual_legal->total_amount ?>"
 									order_number="<?php echo $annual_legal->order_number ?>">
-								    <span> Pay Now </span>
+								    <span> Buy Now </span>
 								</button>
 
 							</div>
@@ -329,11 +336,11 @@
 
 						<div class="three columns">
 
-							<div class="pay-content-box">
+							<div class="pay-content-box box-small">
 
 								<h3> Annual CMA </h3>
 
-								<h3> 11 x $9.95 </h3>
+								<h3 class="price-header"> <span>$</span>9.95<span>x11</span> </h3>
 
 								<button api_login_id="<?php echo APIKEY ?>"
 									method="sale"
@@ -346,7 +353,7 @@
 									callback="oncallback_sale"
 									total_amount="<?php echo $annual_cma->total_amount ?>"
 									order_number="<?php echo $annual_cma->order_number ?>">
-								    <span> Pay Now </span>
+								    <span> Buy Now </span>
 								</button>
 
 							</div>
@@ -380,13 +387,9 @@
 
 		</div>
 
-    <div id="footer">
-      <?php include('templates/footer.html'); ?>
-    </div>
-
 	</body>
 
 	<script type="text/javascript" src="https://code.jquery.com/jquery-1.11.3.min.js"> </script>
-	<script type="text/javascript" src="reoswp/js/app.js"> </script>
+	<script type="text/javascript" src="js/app.js"> </script>
 
 </html>
