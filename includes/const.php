367c5b5f83fcc6688435ec02236583b7c8a23731
Blayne <blayneholland@outlook.com>
2015-11-23 17:31:07 -0500

    changed something slightly with const

--- includes/const.php
+++ includes/const.php
@@ -3,10 +3,6 @@
 # Set UTC time. If time isn't correct tranaction will not work send.
 date_default_timezone_set('UTC');
 
-# define("APIKEY", "4fNd2kU25W");
-# define("TKEY", "8DlE01kR"); # secret Transaction key, do not expose to public
-
-
 define("APIKEY", "y62W6EUoi5");
 define("TKEY", "2nR8S4lC"); # secret Transaction key, do not expose to public
 
